836b1c6c99627d8acbf71146734f83f5dc0a3812
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-01-28 20:53:10 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+	const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+        var prefix = 'f!'
+     var msg = message.content;
+	 
+	 
+
+     if (msg === prefix + 'help') {
+
+         message.channel.send(' Want to learn about commands? Need help? Visit https://fortcord.com \n \n Plus, you can discover the whole website with all info about the bot!!! \n If you have any questions, contact me by mail using  xxgamerxx.ca@gmail.com');
+		 
+	
+		 
+     }
+});
+
+client.login('NTM2NTk0ODMxNjQ5Mjc1OTI0.Dy_c1w.T07pqYjt3qUQg8MSGErMiDBGKmU');
+
