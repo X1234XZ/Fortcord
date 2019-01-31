29cdafa955009572aa3c2c35af744607445696a3
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-01-30 21:48:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,49 @@
-	const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-   console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-        var prefix = 'f!'
-     var msg = message.content;
+    var prefix = 'f!'
+    var msg = message.content;
+
+
+
+
+    if (msg === prefix + 'help') {
+
+        message.channel.send(' Want to learn about commands? Need help?  Visit http://fortcord.com/index.php/commands/ \n ***This site is incomplete and be sure to click on the link and not the links in the text box.** \n \n Plus, you can discover the whole website with all info about the bot!!! \n If you have any questions, contact me by mail using  **xxgamerxx.ca@gmail.com** or on discord using \n **!!![BetterThanYou]!!!#1466**. \n \n *If you want to donate tap* `f!donate` *and it will give you the donate link. ')
+
+    }
+
+    if (msg === prefix + 'donate') {
+
+        message.channel.send(' want to donate? No problem, go on this link and buy the pack you want! \n \n https://donatebot.io/checkout/539435332198858753 \n Enjoy! \n \n ***Contact me if there is any problems.**');
+
+    }
+
+    if (msg === prefix + 'server') {
+
+        message.channel.send('**Join the support server!!!** \n You can also chat with others. https://discord.gg/HG4BUQD')
+
+    }
+
+    if (msg === prefix + 'invite') {
+
+        message.channel.send('**Invite the bot to other servers!!!** \n Link: https://discordapp.com/oauth2/authorize?client_id=536594831649275924&scope=bot&permissions=252928')
+
+    }
+
+    if (msg === prefix + 'vote') {
+
+        message.channel.send("Hello " + message.author.toString() + ", and welcome!");
+
+    }
 
 })
-	
-	 
-	 
 
-     if (msg === prefix + 'help') {
-		 
-		 message.channel.send(' Want to learn about commands? Need help?  Visit http://fortcord.com/index.php/commands/ \n ***This site is incomplete and be sure to click on the link and not the links in the text box.** \n \n Plus, you can discover the whole website with all info about the bot!!! \n If you have any questions, contact me by mail using  **xxgamerxx.ca@gmail.com** or on discord using \n **!!![BetterThanYou]!!!#1466**. \n \n *If you want to donate tap* `f!donate` *and it will give you the donate link. ')
-		 
-	 }
-	 
-	 if (msg === prefix + 'donate') {
 
-	 message.channel.send(' want to donate? No problem, go on this link and buy the pack you want! \n \n https://donatebot.io/checkout/539435332198858753 \n Enjoy! \n \n ***Contact me if there is any problems.**');
-		 
-	
-		 
-     }
-	 
-	 if (msg === prefix + 'server') {
-		 
-		 message.channel.send('**Join the support server!!!** \n You can also chat with others. https://discord.gg/HG4BUQD')
-		 
-	 }
-	
-	if (msg === prefix + 'invite') {
-	
-	message.channel.send('**Invite the bot to other servers!!!** \n Link: https://discordapp.com/oauth2/authorize?client_id=536594831649275924&scope=bot&permissions=252928')
-	
-	}
-	
-	if (msg === prefix + 'vote') {
-	
-	message.channel.send("Hello " + message.author.toString() + ", and welcome!");
-		
-	}
-	
-	
-	
-	
 
 client.login('NTM2NTk0ODMxNjQ5Mjc1OTI0.Dy_c1w.T07pqYjt3qUQg8MSGErMiDBGKmU');
-
