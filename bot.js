dd5f16bbf13c35b9e197856c949e8a9aac055d3e
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-04 21:54:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,23 @@ client.on('message', message => {
 
         message.channel.send(" " + message.author.toString() + " Sorry about that, the command is not currently unavailable. \n **Please wait patiently, thank you!**");
     }
+    
+    if (message.content === 'f!profile') {
+      let botembed = new Discord.RichEmbed()
+                .setTitle("**__Exoly User Profile__**")
+                .setTimestamp(new Date())
+                .setColor("#4286f4")
+                .setFooter("Exolia", `${bot.user.avatarURL}`)
+                .setThumbnail(`${message.author.avatarURL}`)
+                .addField("Username :", `${message.author.username}`, inline = true)
+                .addField("Exolytes :", "|---|", inline = true)
+                .addField("Played Time :", "|---|", inline = true)
+                .addField("Faction :", "Armada", inline = true);
+       if (shouldResponseTo(message)) {
+           message.delete()
+           return message.channel.send(botembed);
+       }
+    }
 
 })
 
