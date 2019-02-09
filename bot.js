0452c6bd855a08aa9bd1d5656f7f183c3087f70c
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-09 13:33:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,16 +47,10 @@ client.on('message', message => {
     if (msg === prefix + 'userinfo'){
     
    let user = message.mentions.users.first() || message.author;
-    const joinDiscord = moment(user.createdAt).format('llll');
-    const joinServer = moment(user.joinedAt).format('llll');
+   
     let embed = new Discord.RichEmbed()
         .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setDescription(`${user}`)
-        .setColor(`RANDOM`)
-        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Status:', user.presence.status, true)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp();
+        
 
     message.channel.send({ embed: embed });
     return;
