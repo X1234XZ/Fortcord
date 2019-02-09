ed5d765171bf3fe549902624d4b7cef0104f5e76
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-09 14:14:15 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,22 +44,15 @@ client.on('message', message => {
     }
     
     if (msg === prefix + 'userinfo'){
-    
-  message.channel.send({embed: {
+        
+    let user = message.mentions.users.first() || message.author;
+    let embed = new Discord.RichEmbed()
+     
     color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      }]
-    
+    .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL) 
+    .addField("Level: ", " **10**") message.channel.send({ embed: embed }); return; }
+    }
 })
-}
+
 
 client.login(auth.token);
