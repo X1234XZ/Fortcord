b95b96e9d8b8276087c013f4a174c1c95ceb2720
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-09 13:36:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on('message', message => {
    
     let embed = new Discord.RichEmbed()
         .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        
+        .addField("Hello")
 
     message.channel.send({ embed: embed });
     return;
