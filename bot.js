7f2b48fd1b2bd07dea583c044bea42c6a2d1c7aa
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-09 14:36:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,25 +48,16 @@ client.on('message', message => {
         message.channel.send({embed: {
     color: 3447003,
     author: {
+        
       name: user.username,
       icon_url: user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    
     fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+        name: "Level: ",
+        value: "**2**."
       },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
+          ],
     timestamp: new Date(),
     
   }
