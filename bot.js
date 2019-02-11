bcdc95027159e09349a2b657349c41b6ed8bcedf
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-11 14:25:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', message => {
     
     fields: [{
         name: "Profile",
-        value: "**Level:** 20 \n **Experience:** 2697/3000 \n **Clan:** None \n **Tier:** 14 \n **Tier XP:** 3/10 \n **Balance:** 328755 <:c_:emoji_id>"
+        value: "**Level:** 20 \n **Experience:** 2697/3000 \n **Clan:** None \n **Tier:** 14 \n **Tier XP:** 3/10 \n **Balance:** 328755 <:thinking:>"
       },
           ],
             
