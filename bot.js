5e5d6314cceeef849ac319a802e57ea21f6f84e8
X1234XZ <45948509+X1234XZ@users.noreply.github.com>
2019-02-15 17:23:15 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 var auth = require('./config.json');
 const client = new Discord.Client();
-client.user.setActivity("Playing Fortcord with everyone"); 
+client.user.setActivity("Playing Fortcord with **everyone**!!!"); 
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
